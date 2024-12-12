7ccbfabf3cf15a6d81b0b463678a0f0b156b6e3f
Φ Bui <bilphui@gmail.com>
2024-12-11 18:10:15 -0800

    mask finder

--- implementation/controller-server/receiver.py
+++ implementation/controller-server/receiver.py
@@ -250,7 +250,7 @@ if __name__ == "__main__":
         spheros = [
             {"id": "SB-2E86", "color": "#FF00FF", "ready": False},
             {"id": "SB-4844", "color": "#FF0000", "ready": False},
-            {"id": "SB-7104", "color": "#00FF00", "ready": False},
+            {"id": "SB-7104", "color": "#035900", "ready": False},
             {"id": "SB-D8B2", "color": "#00FFFF", "ready": False},
             {"id": "SB-E12C", "color": "#FFFF00", "ready": False}
         ]
